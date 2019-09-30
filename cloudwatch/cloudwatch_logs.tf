1e24190026cacdf4b57681f9a977b7bd3ac39c36
Murtaza Usman <murtaza.usman@intercom.io>
2019-09-30 09:53:40 +0100

    Update cloudwatch_logs.tf

--- cloudwatch/cloudwatch_logs.tf
+++ cloudwatch/cloudwatch_logs.tf
@@ -1,1 +1,23 @@
+resource "aws_cloudwatch_log_metric_filter" "app_already_running" {
+  name           = "AppAlreadyRunningError"
+  pattern        = "App already running"
+  log_group_name = "/aws/lambda/vendors_admin_app"
 
+  metric_transformation {
+    name      = "AppAlreadyRunningError"
+    namespace = "StreamAlert"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "task_timed_out" {
+  name           = "TaskTimedOutError"
+  pattern        = "Task timed out"
+  log_group_name = "/aws/lambda/vendors_admin_app"
+
+  metric_transformation {
+    name      = "TaskTimedOutError"
+    namespace = "StreamAlert"
+    value     = "1"
+  }
+}
